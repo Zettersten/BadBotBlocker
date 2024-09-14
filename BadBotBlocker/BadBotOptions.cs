c8961223da64959e30598a3e4508aa1daaad4ef9
Erik Zettersten <erik@zettersten.com>
2024-09-14 17:00:28 -0400

    Update BadBotOptions.cs

--- BadBotBlocker/BadBotOptions.cs
+++ BadBotBlocker/BadBotOptions.cs
@@ -82,40 +82,263 @@ public sealed class BadBotOptions
         return this;
     }
 
-    /// <summary>
+    
+
+  
+/// <summary>
     /// Loads the default bad bot patterns.
     /// </summary>
     private void LoadDefaultBadBotPatterns()
+{
+    var patterns = new[]
     {
-        var patterns = new[]
-        {
-            "^Aboundex",
-            "^80legs",
-            // ... (remaining patterns omitted for brevity)
-            "Baiduspider",
-            "Yandex",
-        };
+        "^Aboundex",
+        "^80legs",
+        "^360Spider",
+        "^Java",
+        "^Cogentbot",
+        "^Alexibot",
+        "^asterias",
+        "^attach",
+        "^BackDoorBot",
+        "^BackWeb",
+        "Bandit",
+        "^BatchFTP",
+        "^Bigfoot",
+        "^Black.Hole",
+        "^BlackWidow",
+        "^BlowFish",
+        "^BotALot",
+        "Buddy",
+        "^BuiltBotTough",
+        "^Bullseye",
+        "^BunnySlippers",
+        "^Cegbfeieh",
+        "^CheeseBot",
+        "^CherryPicker",
+        "^ChinaClaw",
+        "Collector",
+        "Copier",
+        "^CopyRightCheck",
+        "^cosmos",
+        "^Crescent",
+        "^Custo",
+        "^AIBOT",
+        "^DISCo",
+        "^DIIbot",
+        "^DittoSpyder",
+        "^Download Demon",
+        "^Download Devil",
+        "^Download Wonder",
+        "^dragonfly",
+        "^Drip",
+        "^eCatch",
+        "^EasyDL",
+        "^ebingbong",
+        "^EirGrabber",
+        "^EmailCollector",
+        "^EmailSiphon",
+        "^EmailWolf",
+        "^EroCrawler",
+        "^Exabot",
+        "^Express WebPictures",
+        "Extractor",
+        "^EyeNetIE",
+        "^Foobot",
+        "^flunky",
+        "^FrontPage",
+        "^Go-Ahead-Got-It",
+        "^gotit",
+        "^GrabNet",
+        "^Grafula",
+        "^Harvest",
+        "^hloader",
+        "^HMView",
+        "^HTTrack",
+        "^humanlinks",
+        "^IlseBot",
+        "^Image Stripper",
+        "^Image Sucker",
+        "Indy Library",
+        "^InfoNaviRobot",
+        "^InfoTekies",
+        "^Intelliseek",
+        "^InterGET",
+        "^Internet Ninja",
+        "^Iria",
+        "^Jakarta",
+        "^JennyBot",
+        "^JetCar",
+        "^JOC",
+        "^JustView",
+        "^Jyxobot",
+        "^Kenjin.Spider",
+        "^Keyword.Density",
+        "^larbin",
+        "^LexiBot",
+        "^lftp",
+        "^libWeb/clsHTTP",
+        "^likse",
+        "^LinkextractorPro",
+        "^LinkScan/8.1a.Unix",
+        "^LNSpiderguy",
+        "^LinkWalker",
+        "^lwp-trivial",
+        "^LWP::Simple",
+        "^Magnet",
+        "^Mag-Net",
+        "^MarkWatch",
+        "^Mass Downloader",
+        "^Mata.Hari",
+        "^Memo",
+        "^Microsoft.URL",
+        "^Microsoft URL Control",
+        "^MIDown tool",
+        "^MIIxpc",
+        "^Mirror",
+        "^Missigua Locator",
+        "^Mister PiX",
+        "^moget",
+        "^Mozilla/3.Mozilla/2.01",
+        "^Mozilla.*NEWT",
+        "^NAMEPROTECT",
+        "^Navroad",
+        "^NearSite",
+        "^NetAnts",
+        "^Netcraft",
+        "^NetMechanic",
+        "^NetSpider",
+        "^Net Vampire",
+        "^NetZIP",
+        "^NextGenSearchBot",
+        "^NG",
+        "^NICErsPRO",
+        "^niki-bot",
+        "^NimbleCrawler",
+        "^Ninja",
+        "^NPbot",
+        "^Octopus",
+        "^Offline Explorer",
+        "^Offline Navigator",
+        "^Openfind",
+        "^OutfoxBot",
+        "^PageGrabber",
+        "^Papa Foto",
+        "^pavuk",
+        "^pcBrowser",
+        "^PHP version tracker",
+        "^Pockey",
+        "^ProPowerBot/2.14",
+        "^ProWebWalker",
+        "^psbot",
+        "^Pump",
+        "^QueryN.Metasearch",
+        "^RealDownload",
+        "Reaper",
+        "Recorder",
+        "^ReGet",
+        "^RepoMonkey",
+        "^RMA",
+        "Siphon",
+        "^SiteSnagger",
+        "^SlySearch",
+        "^SmartDownload",
+        "^Snake",
+        "^Snapbot",
+        "^Snoopy",
+        "^sogou",
+        "^SpaceBison",
+        "^SpankBot",
+        "^spanner",
+        "^Sqworm",
+        "Stripper",
+        "Sucker",
+        "^SuperBot",
+        "^SuperHTTP",
+        "^Surfbot",
+        "^suzuran",
+        "^Szukacz/1.4",
+        "^tAkeOut",
+        "^Teleport",
+        "^Telesoft",
+        "^TurnitinBot/1.5",
+        "^The.Intraformant",
+        "^TheNomad",
+        "^TightTwatBot",
+        "^Titan",
+        "^True_Robot",
+        "^turingos",
+        "^TurnitinBot",
+        "^URLy.Warning",
+        "^Vacuum",
+        "^VCI",
+        "^VoidEYE",
+        "^Web Image Collector",
+        "^Web Sucker",
+        "^WebAuto",
+        "^WebBandit",
+        "^Webclipping.com",
+        "^WebCopier",
+        "^WebEMailExtrac.*",
+        "^WebEnhancer",
+        "^WebFetch",
+        "^WebGo IS",
+        "^Web.Image.Collector",
+        "^WebLeacher",
+        "^WebmasterWorldForumBot",
+        "^WebReaper",
+        "^WebSauger",
+        "^Website eXtractor",
+        "^Website Quester",
+        "^Webster",
+        "^WebStripper",
+        "^WebWhacker",
+        "^WebZIP",
+        "Whacker",
+        "^Widow",
+        "^WISENutbot",
+        "^WWWOFFLE",
+        "^WWW-Collector-E",
+        "^Xaldon",
+        "^Xenu",
+        "^Zeus",
+        "ZmEu",
+        "^Zyborg",
+        // Vulnerability Scanners
+        "Acunetix",
+        "FHscan",
+        // Aggressive Chinese Search Engine
+        "Baiduspider",
+        // Aggressive Russian Search Engine
+        "Yandex"
+    };
 
-        this.BadBotPatterns.AddRange(patterns);
-    }
+    BadBotPatterns.AddRange(patterns);
+}
 
-    /// <summary>
+  /// <summary>
     /// Loads the default blocked IP ranges.
     /// </summary>
-    private void LoadDefaultBlockedIPRanges()
+private void LoadDefaultBlockedIPRanges()
+{
+    var ipRanges = new[]
     {
-        var ipRanges = new[]
-        {
-            // Cyveillance
-            "38.100.19.8/29",
-            "38.100.21.0/24",
-            // ... (remaining IP ranges omitted for brevity)
-            "65.222.185.72/29",
-        };
+        // Cyveillance
+        "38.100.19.8/29",
+        "38.100.21.0/24",
+        "38.100.41.64/26",
+        "38.105.71.0/25",
+        "38.105.83.0/27",
+        "38.112.21.140/30",
+        "38.118.42.32/29",
+        "65.213.208.128/27",
+        "65.222.176.96/27",
+        "65.222.185.72/29"
+    };
 
-        foreach (var cidr in ipRanges)
-        {
-            this.AddBlockedIPRange(cidr);
-        }
+    foreach (var cidr in ipRanges)
+    {
+        AddBlockedIPRange(cidr);
     }
+}
 }
